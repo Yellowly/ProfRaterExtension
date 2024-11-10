0f38bb1c1a44c8b8db32d347fe8cf68891bd4bd3
Riteshbhirud@gmail.com <riteshbhirud@gmail.com>
2024-11-09 19:27:57 -0500

    Co-authored-by: aryand15 <aryand15@users.noreply.github.com> Co-authored-by: rp1lla1 <rp1lla1@users.noreply.github.com> Co-authored-by: Ian <Yellowly@users.noreply.github.com>

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 let toptags = []
 async function get_ratemyprof_link(prof_name){
   console.log("INSIDE GET LINK FUNCTION")
-  prof_name = prof_name.toLowerCase().replace(" ","%20")
+  // reverse prof name when searching for their rmp link to ensure more accurate results
+  prof_name = prof_name.toLowerCase().split(" ").reverse().join("%20");
+  console.log("SEARCH:", prof_name)
   // fetch ratemyprofessor search results
   let response = await fetch("https://www.ratemyprofessors.com/search/professors/1513?q="+prof_name)
   if (response.status !== 200) {
@@ -95,6 +97,15 @@ function get_ratemyprof_toptags(page) {
   return toptags;
 }
 
+function get_ratemyprof_numratings(page) {
+  let startidx = page.indexOf("\"RatingValue__NumRatings-qw8sqy-0 jMkisx")
+  startidx = page.indexOf("<a", startidx)
+  startidx = page.indexOf(">", startidx)+1
+  middleidx = page.indexOf("<!-- -->", startidx)
+  endidx = page.indexOf("</a>", startidx);
+  
+  return page.substring(startidx, middleidx) + page.substring(middleidx+8, endidx);
+}
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
@@ -104,10 +115,11 @@ chrome.runtime.onInstalled.addListener(() => {
 
 let receivedArray;
 let profLinks;
+let prof_cache = {}
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {(async () => {
     if (message.type === "SEND_ARRAY") {
       const receivedArray = message.data;
-      let profLinks = "";
+      profLinks = "";
       let responses = []
       // Define an async function to process each professor
       for (const profName of receivedArray) {
@@ -115,7 +127,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {(async 
           // const link = await get_ratemyprof_link(profName);
           // console.log("Link:", link);
           // profLinks += `${link},`;
-
+          if(prof_cache[profName]===undefined){
           
           const pageData = await get_ratemyprof_page(profName);
           const profPage = pageData.pagetext;
@@ -123,12 +135,25 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {(async 
           
 
           const professor = await get_div_content(pageData.pagetext, "title data-react-helmet=\"true\"").content.trim().split(" at")[0];
-          if (profName !== professor.trim().toLowerCase()) {
+          
+          if(prof_cache[profName]===undefined){
+          const processedProfName = profName.trim().toLowerCase()
+          const ogfullname = processedProfName.split(" ")
+          const processedProfessor = professor.trim().toLowerCase()
+          const proffullname = processedProfessor.split(" ")
+          // as long as the full names aren't substrinngs of each other, 
+          // skip this prof if the last names are not equal, or first names are not substrings of each other
+          if (!(processedProfessor.includes(processedProfName) || processedProfName.includes(processedProfessor)) && (ogfullname[1]!==proffullname[1] || (!proffullname[0].includes(ogfullname[0]) && !ogfullname[0].includes(proffullname[0])))){
+            console.log(ogfullname);
+            console.log(proffullname);
             continue;
           }
 
           const rating = await get_ratemyprof_rating(profPage);
           //console.log("Overall Rating:", rating);
+
+          const numratings = await get_ratemyprof_numratings(profPage);
+
           const reviews = await get_ratemyprof_reviews(profPage);
           //console.log("Reviews:", reviews);
           
@@ -141,8 +166,8 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {(async 
 
           // console.log("Link:",pageData.link);
 
-
-          let prompt = `I need to summarize the reviews of professor ${professor}. I have scraped the following data: They have an average rating of ${rating} out of 5, ${takeAgainAndDifficulty[0]} of students would take the course again and students rated the professor's average difficulty as ${takeAgainAndDifficulty[1]} out of 5. Here are the most common attributes/tags about the professor ${toptags}. Finally, here are some written reviews that students had about the professors: ${reviews}\n\n Write a short blurb that uses all of this information to summarize the reviews and what you can expect from the class. Please do not sugarcoat your review as it will be the primary factor in students' course selection.`
+          
+          let prompt = `I need to summarize the reviews of professor ${professor}. I have scraped the following data: They have an average rating of ${rating} out of 5, based on ${numratings} ratings. ${takeAgainAndDifficulty[0]} of students would take the course again and students rated the professor's average difficulty as ${takeAgainAndDifficulty[1]} out of 5. Here are the most common attributes/tags about the professor ${toptags}. Finally, here are some written reviews that students had about the professors: ${reviews}\n\n Write a short blurb that uses all of this information to summarize the reviews and what they can expect from the class. Please do not sugarcoat your review as it will be the primary factor in students' course selection. Responses should be 3 sentences max or around 50-60 words. Note that these reviews come across classes, so your summary should focus on professor rather than features of the class (as we don't know what class the student is taking with the prof)`
           
           const apiKey = "AIzaSyAEK-YG3823HWIMcqUBFh4ys4pnNs0SUdA";
           const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
@@ -160,9 +185,22 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {(async 
           });
           let data = await response.json()
           console.log(data)
-          let summary = data.candidates[0].content.parts[0].text;
-          responses.push({prof_rating: rating, prof_name: professor, link: pageData.link, summary: summary})
-          
+          let summary = "Error generating summary"
+          console.log(response)
+          try{
+           summary = data.candidates[0].content.parts[0].text;
+          } catch (error){
+            console.error("Error processing professor:", profName, error);
+          }
+          let temp = {prof_rating: rating, prof_num_ratings: numratings, prof_name: professor, link: pageData.link, summary: summary}
+          responses.push(temp)
+          prof_cache[professor]=temp
+          }else{
+            responses.push(prof_cache[professor])
+          }
+          }else{
+            responses.push(prof_cache[profName])
+          }
 
         } catch (error) {
           console.error("Error processing professor:", profName, error);
@@ -170,7 +208,20 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {(async 
         //profName/name toptags, takeAgainAndDifficulty, reviews, rating
     }
     console.log(responses)
-    sendResponse({all_data: responses});
+    if(responses.length>0){
+      responses.sort((a, b) => {
+        // First, sort by prof_rating in descending order
+        if (b.prof_rating !== a.prof_rating) {
+          return b.prof_rating - a.prof_rating;
+        }
+        // If prof_rating is the same, sort by prof_num_ratings in descending order
+        return b.prof_num_ratings - a.prof_num_ratings;
+      });
+      sendResponse({all_data: responses});
+    } else{
+      //responses = []
+      sendResponse({message: "None of the professors for this class have a RateMyProfessor page. You're cooked!"})
+    }
 }
 })(); return true;});
 //console.log("received Array", receivedArray)
--- myscript2.js
+++ myscript2.js
@@ -1,25 +1,72 @@
 function getSelectionText() {
-    let text = "";
+  let text = "";
 
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
+  if (window.getSelection) {
+      text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+      text = document.selection.createRange().text;
+  }
 
-    return text;
+  return text;
+}
+
+function dragElement(elmnt) {
+var offsetX = 0, offsetY = 0;
+if (document.getElementById(elmnt.id + "header")) {
+  // if present, the header is where you move the DIV from:
+  document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+} else {
+  // otherwise, move the DIV from anywhere inside the DIV:
+  elmnt.onmousedown = dragMouseDown;
+}
+
+function dragMouseDown(e) {
+  e = e || window.event;
+  e.preventDefault();
+  // get the mouse cursor position at startup:
+  offsetX = e.clientX - elmnt.getBoundingClientRect().left;
+  offsetY = e.clientY - elmnt.getBoundingClientRect().top;
+  document.onmouseup = closeDragElement;
+  // call a function whenever the cursor moves:
+  document.onmousemove = elementDrag;
+}
+
+function elementDrag(e) {
+  e = e || window.event;
+  e.preventDefault();
+  // calculate the new cursor position:
+  const newX = e.clientX - offsetX;
+  const newY = e.clientY - offsetY;
+  // set the element's new position:
+  elmnt.style.left=newX+"px";
+  elmnt.style.top=newY+"px";
+}
+
+function closeDragElement() {
+  // stop moving when mouse button is released:
+  document.onmouseup = null;
+  document.onmousemove = null;
+}
 }
 
 const popupthingy = document.createElement("div")
 
 popupthingy.setAttribute("popover","")
 popupthingy.setAttribute("class","ratemyprofpopup")
+popupthingy.setAttribute("tabindex","0")
+popupthingy.style.display="none"
 document.body.appendChild(popupthingy);
+
+const popupdata = document.createElement("div")
+popupdata.setAttribute("class","profpopupdata")
+popupthingy.appendChild(popupdata);
 
 const popuplink = document.createElement("a");
 popuplink.setAttribute("href","https://www.ratemyprofessors.com/")
 popuplink.setAttribute("target","_blank")
-popupthingy.appendChild(popuplink);
+const popupNum = document.createElement("p")
+popupdata.appendChild(popupNum)
+popupdata.appendChild(popuplink);
 
 const popupheader = document.createElement("h3");
 popupheader.textContent="name"
@@ -27,40 +74,98 @@ popuplink.appendChild(popupheader);
 
 const popupscore = document.createElement("h1");
 popupscore.textContent="5.0"
-popupthingy.appendChild(popupscore);
+popupdata.appendChild(popupscore);
+
+const popupicon = document.createElement("img");
+popupicon.setAttribute("src","https://i.pinimg.com/736x/8b/c7/6d/8bc76db022e08779c4eb5f27cc2f49ac.jpg")
+popupthingy.appendChild(popupicon);
+
+const popupnumelements = document.createElement("p");
+
+
+popupnumelements.textContent = "0";
+
+popupdata.appendChild(popupnumelements)
+
 
 const popupcontent = document.createElement("p");
 popupcontent.textContent="none"
+popupcontent.setAttribute("class","profpopupcontent")
 popupthingy.appendChild(popupcontent);
 
 
-function show_popup(response){
-    popupthingy.style.left=`${10}px`;
-    popupthingy.style.top=`${10}px`;
-    popupscore.textContent = response.prof_rating;
-    popupheader.textContent = response.prof_name;
-    popupcontent.textContent = response.summary;
-    popuplink.setAttribute("href",response.link)
-    popupthingy.showPopover();
+let reply;
+
+function show_popup(response,iter=10){
+  
+  popupthingy.style.left=`${10}px`;
+  popupthingy.style.top=`${10}px`;
+  console.log(reply)
+  if(iter ===0){
+      iter = 1;
+  }
+  popupNum.textContent = `Option ${iter} of ${reply.all_data.length}`
+  popupscore.textContent = `${response.prof_rating}/5`;
+  popupnumelements.textContent = `Based on ${response.prof_num_ratings}`;
+  popupheader.textContent = response.prof_name;
+  popupcontent.textContent = response.summary;
+  popuplink.setAttribute("href",response.link)
+  popuplink.focus();
+  popupthingy.showPopover();
+  popupthingy.style.display = "grid";
 }
+
+function show_loading_popup(){
+  popupthingy.style.left=`${10}px`;
+  popupthingy.style.top=`${10}px`;
+
+  popupNum.textContent = ""
+  popupscore.textContent = "";
+  popupnumelements.textContent = "";
+  popupheader.textContent = "Loading...";
+  popupcontent.textContent = "";
+  popuplink.setAttribute("href","")
+  popuplink.focus();
+  popupthingy.showPopover();
+  popupthingy.style.display = "grid";
+
+
+}
+
+dragElement(popupthingy);
 
 let stored_responses = []
 let curr_displayed = 0;
 
 document.onkeydown = function(e) {
-    if(e.keyCode==39){
-        curr_displayed=Math.min(++curr_displayed,stored_responses.length-1)
-        show_popup(stored_responses[curr_displayed]);
-    }else if (e.keyCode==37){
-        curr_displayed=Math.max(--curr_displayed, 0)
-        show_popup(stored_responses[curr_displayed]);
-    }else if (e.keyCode=="\\"){
-        chrome.runtime.sendMessage({ type: "SEND_ARRAY", data: [getSelectionText()] }, (response) => {
-            console.log("Response from background:", response);
-            stored_responses=response.all_data;
-            curr_displayed=0;
-            show_popup(stored_responses[0]);
-        });
-    }
+  if(e.keyCode==39){
+      curr_displayed=Math.min(++curr_displayed,stored_responses.length-1)
+      show_popup(stored_responses[curr_displayed]);
+  }else if (e.keyCode==37){
+      curr_displayed=Math.max(--curr_displayed, 0)
+      show_popup(stored_responses[curr_displayed]);
+  }else if (e.key=="\\"){
+    show_loading_popup();
+      chrome.runtime.sendMessage({ type: "SEND_ARRAY", data: [getSelectionText()] }, (response) => {
+          console.log("Response from background:", response);
+          stored_responses=response.all_data;
+          reply = response;
+          curr_displayed=0;
+          show_popup(stored_responses[0]);
+      });
+  }else if (e.keyCode==27){
+      popupthingy.style.display="none";
+  }
+}
+
+document.onmousedown = function(){
+  if (getSelectionText()==""){
+      popupthingy.style.display="none";
+  }
+}
+
+popupthingy.onblur = function(){
+  popupthingy.hidePopover()
+  popupthingy.style.display = "none";
 }
 
